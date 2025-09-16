0a5d21d42021dfc5a0c1327c53e177d692bbe883
Okaroni Purity <nekesapurity20@gmail.com>
2025-09-16 21:28:21 +0300

    completed lab

--- server/models.py
+++ server/models.py
@@ -3,41 +3,48 @@ from sqlalchemy import MetaData
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
+# Naming convention for foreign keys
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 
 db = SQLAlchemy(metadata=metadata)
 
-class Customer(db.Model):
+class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
-    # Add relationship to Review
     reviews = db.relationship('Review', back_populates='customer', cascade='all, delete-orphan')
+
+    # Association proxy to get items reviewed by the customer
+    items = association_proxy('reviews', 'item')
+
+    # Prevent infinite recursion when serializing
+    serialize_rules = ('-reviews.customer', '-reviews.item')
 
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
 
 
-class Item(db.Model):
+class Item(db.Model, SerializerMixin):
     __tablename__ = 'items'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
-    # Add relationship to Review
     reviews = db.relationship('Review', back_populates='item', cascade='all, delete-orphan')
+
+    # Prevent infinite recursion when serializing
+    serialize_rules = ('-reviews.item', '-reviews.customer')
 
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
 
 
-# New Review model
-class Review(db.Model):
+class Review(db.Model, SerializerMixin):
     __tablename__ = 'reviews'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -47,6 +54,9 @@ class Review(db.Model):
 
     customer = db.relationship('Customer', back_populates='reviews')
     item = db.relationship('Item', back_populates='reviews')
+
+    # Prevent infinite recursion when serializing
+    serialize_rules = ('-customer.reviews', '-item.reviews')
 
     def __repr__(self):
         return f'<Review {self.id}, Customer {self.customer_id}, Item {self.item_id}, "{self.comment}">'
